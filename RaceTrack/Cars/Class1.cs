0e4cefb696efd4071b43e952a61eda298c091d2e
Allie Scherf <ascherf2@my.wctc.edu>
2022-03-07 18:47:42 -0600

    First commit made classes

---
+++ RaceTrack/Cars/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+
+{
+    public class IceCreamTruck : RaceCar
+    {
+        public IceCreamTruck()
+        {
+            Name = "Ice Cream Truck";
+            TopSpeed = 65;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} engine starts");
+        }
+    }
+}
+
